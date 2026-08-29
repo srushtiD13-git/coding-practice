d21c9b2f3c258ded85f9b0326090d559c12b5e75
srushtiD13-git <srushtidalvi99@gmail.com>
2026-08-29 12:22:20 +0530

    Time: 0 ms (100%), Space: 43.2 MB (28.27%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -13,28 +13,6 @@ class Solution {
         }
       }
 
-      
-
       return true;
-    }
-
-    public void printArr(int[] arr)
-    {
-        for (int i = 0; i < arr.length; i++) {
-    System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-
-    }
-
-    public boolean isSorted(int[] arr)
-    {
-        for(int l=0;l<arr.length-1;l++)
-        {
-            if(arr[l]>arr[l+1])
-            {return false;}
-        }
-
-        return true;
     }
 }
