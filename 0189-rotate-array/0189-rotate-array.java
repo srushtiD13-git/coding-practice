1ec08286e787fdd1da3d323f30047575fabc2c8b
srushtiD13-git <srushtidalvi99@gmail.com>
2026-09-01 11:58:24 +0530

    Time: 3053 ms (5.76%), Space: 129.5 MB (24.14%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        if(nums.length>1)
+        {
+            k = k % nums.length;
+            reverseArray(0,nums.length-1,nums);
+        print(nums);
+
+        reverseArray(0,k-1,nums);
+        reverseArray(k,nums.length-1,nums);
+        }
+        
+    }
+
+    public void reverseArray(int left, int right, int[] nums)
+    {
+        while(right>left)
+        {
+            int temp=nums[right];
+            nums[right]=nums[left];
+            nums[left]=temp;
+            right--;
+            left++;
+        }
+    }
+
+    public void print(int[] nums)
+    {
+        for(int i=0;i<nums.length-1;i++)
+        {
+            System.out.print(nums[i]+" ");
+        }
+    }
+        
+}
