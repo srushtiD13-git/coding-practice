a955c8c3a5c8d6c73d330dc9c67b8aacba7c08d6
srushtiD13-git <srushtidalvi99@gmail.com>
2026-09-03 23:51:51 +0530

    Time: 2 ms (91.84%), Space: 47.9 MB (20.26%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length > 1)
+        {
+            int j=0;
+            for(int i=0;i<nums.length;i++)
+            {
+                if(nums[i]!=0)
+                {nums[j]=nums[i];
+                    j++;
+                }
+            }
+
+            for(int k=j;k<nums.length;k++)
+            {
+                nums[k]=0;
+            }
+        }
+    }
+}
