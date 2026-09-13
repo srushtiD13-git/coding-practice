2c7d3fe015abbfa097cf778538075735ecfa06ee
srushtiD13-git <srushtidalvi99@gmail.com>
2026-09-13 16:12:41 +0530

    Time: 15 ms (24.04%), Space: 46.5 MB (24.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+      Map<Character, Integer> Smap = new HashMap<>();
+      Map<Character, Integer> Tmap = new HashMap<>();
+
+      char[] sArr = s.toCharArray();
+      char[] tArr = t.toCharArray();
+
+      if(sArr.length != tArr.length)
+      {
+        return false;
+      }
+
+      for(int i=0;i<sArr.length;i++)
+      {
+        Smap.put(sArr[i], Smap.getOrDefault(sArr[i], 0)+1);
+       
+      }
+
+      for(int i=0;i<tArr.length;i++)
+      {
+        Tmap.put(tArr[i], Tmap.getOrDefault(tArr[i], 0)+1);
+      }
+
+      if( !Smap.equals(Tmap))
+      {
+        return false;
+      }
+
+
+      System.out.println(sArr[0]);
+
+      return true;  
+    }
+}
