e0c7209b4267cd127b5d979227bb16add6954735
srushtiD13-git <srushtidalvi99@gmail.com>
2026-09-16 22:14:10 +0530

    Time: 10 ms (98.3%), Space: 50.6 MB (6.39%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,29 +1,46 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
+        List<List<Integer>> list = new ArrayList<>();
         int[] arr = new int[k];
-        int arrCount=0;
+
+        for (int i = 0; i <= nums.length; i++) {
+            list.add(new ArrayList<>());
+        }
+  
+
         for(int i=0;i<nums.length;i++)
         {
             map.put(nums[i], map.getOrDefault(nums[i],0)+1);
         }
-
-        for(int i=0;i<k;i++)
+        for (Map.Entry<Integer, Integer> entry : map.entrySet())
         {
-            int maxFreq = 0;
-            int maxNum = 0;
-           for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-                
-                if(entry.getValue() > maxFreq){maxFreq=entry.getValue();
-                maxNum = entry.getKey();}
-
-           }
-
-           map.remove(maxNum);
-           arr[arrCount] = maxNum;
-           arrCount++;
+            list.get(entry.getValue()).add(entry.getKey());
         }
 
+        //System.out.println(list);
+
+        int n = 0;
+
+for (int i = nums.length; i > 0; i--) {
+
+    if (!list.get(i).isEmpty()) {
+
+        for (int j : list.get(i)) {
+            arr[n] = j;
+            n++;
+
+            if (n == k) {
+                break;
+            }
+        }
+    }
+
+    if (n == k) {
+        break;
+    }
+}
         return arr;
+        
     }
 }
